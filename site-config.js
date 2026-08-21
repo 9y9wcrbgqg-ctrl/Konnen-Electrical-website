cb508538c3972a019fc5b7a8409860780725a83a
9y9wcrbgqg-ctrl <9y9wcrbgqg@privaterelay.appleid.com>
2026-08-21 04:12:57 -0700

    Add centralized public business configuration

---
+++ site-config.js
@@ -0,0 +1,10 @@
+window.KONNEN_CONFIG = {
+  company: "Konnen Electrical LLC",
+  displayName: "KÖNNEN ELECTRICAL",
+  phone: "",
+  phoneHref: "",
+  email: "",
+  ccb: "",
+  electricalContractorLicense: "",
+  serviceArea: "Southern Oregon"
+};
